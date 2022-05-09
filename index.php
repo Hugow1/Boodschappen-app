e5bc5ad5db8eece5cc3a5d52a7285dd52cdfe21c
Hugow1 <hugo.winder@gmail.com>
2022-05-09 20:02:15 +0200

    Added the readme

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ if (isset($_GET['addedList'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/output.css">
+    <link rel="stylesheet" href="styles/output.css?v=1">
     <script src="./scripts/functions.js"></script>
     <title>Je lijst</title>
     <link rel="icon" href="images/favicon.ico">
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Mobile grocery shopping 'app'.
+Created an mobile website to create your shopping lists in.
+App includes a shortlist version to quickly add frequently used items to your shopping list.
