ec07973a32bae78afd7ec288988b62c56e69c553
Hugow1 <hugo.winder@gmail.com>
2022-05-30 08:27:52 +0200

    Showed some text on logout page

--- logout.php
+++ logout.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once "./functions.php";
+
 // Initialize the session
 session_start();
 
@@ -11,11 +14,39 @@ if (isset ($_COOKIE['user_id'])) {
     return true;
 }
 
-
 // Destroy the session.
 session_destroy();
 
 // Redirect to login page
 header("location: login.php");
-exit;
+// exit;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logout | Lijs</title>
+    <?php echo '<link rel="stylesheet" href="' . auto_version('styles/output.css') . '" type="text/css">'; ?>
+    <?php echo '<script src="' . auto_version('scripts/functions.js') . '"></script>'; ?>
+    <?php echo '<link rel="manifest" href="' . auto_version('manifest.json') . '">'; ?>
+    <link rel="icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <script type="module">
+        import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+        const el = document.createElement('pwa-update');
+        document.body.appendChild(el);
+    </script>
+</head>
+<body>
+    <header id="header" class="flex items-center justify-between w-full py-5 mt-10 text-3xl px-7">
+        <p class="font-semibold">Je bent uitgelogd</p>
+    </header>
+    <div class="flex flex-col mt-10 px-7">
+        <p>Je bent nu uitgelogd.</p>
+        <p>Je wordt doorgestuurd naar de login pagina.</p>
+        <p>Als je niet automatisch doorgestuurd wordt, klik dan <a class="underline " href="login.php">hier</a>.</p>
+    </div>
+</body>
+</html>
